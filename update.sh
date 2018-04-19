b119a8364a1c515c047d0b7899c969a2483f3293
Shunichi Ikegami <sike.tm@gmail.com>
2018-04-19 17:27:48 +0900

    Add Dockerfile's with chrome

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-for version in 2.3.3 2.3.4 2.3.5 2.4.1 2.4.3 2.5.0; do
-  for variant in phantomjs; do
+for version in 2.3.3 2.3.4 2.3.5 2.4.1 2.4.3 2.4.4 2.5.0 2.5.1; do
+  for variant in phantomjs chrome; do
     mkdir -p ruby/$version/$variant
     ruby -rerb -e "ruby_version='$version'; ERB.new(IO.read('$PWD/ruby/template/$variant/Dockerfile.erb')).run" > "ruby/$version/$variant/Dockerfile"
     # sed -r "s!%%RUBY_VERSION%%!$version!g" "ruby/template/$variant/Dockerfile" > "ruby/$version/$variant/Dockerfile"
